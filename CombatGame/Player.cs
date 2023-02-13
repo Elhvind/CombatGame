21d08f272bafe119f9c1522db9aa41eab5238c0a
Søren Lund <lund14@gmail.com>
2023-02-13 21:22:26 +0100

    Combat game

---
+++ CombatGame/Player.cs
@@ -0,0 +1,48 @@
+﻿namespace CombatGame;
+
+public class Player
+{
+    private static readonly Random RandomNumberGenerator = new();
+
+    public Player(string name)
+    {
+        Name = name;
+        Health = 25;
+        MinDamage = 1;
+        MaxDamage = 6;
+        CriticalChance = 15;
+    }
+
+    public string Name { get; private set; }
+
+    public int Health { get; private set; }
+
+    public int MinDamage { get; private set; }
+
+    public int MaxDamage { get; private set; }
+
+    public int CriticalChance { get; private set; }
+
+    public bool Alive => Health > 0;
+
+    public string Attack(Player other)
+    {
+        var damage = RandomNumberGenerator.Next(MinDamage, MaxDamage);
+
+        var criticalHit = RandomNumberGenerator.Next(0, 100) <= CriticalChance;
+
+
+
+        other.Health -= damage;
+
+        return $"{Name} attacks {other.Name} for {damage} damage.";
+    }
+
+    public string Heal()
+    {
+        var heal = 3;
+        Health += heal;
+
+        return $"{Name} heals for {heal}.";
+    }
+}
--- CombatGame/Program.cs
+++ CombatGame/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CombatGame;
+
+var consoleLog = new List<(int turn, string action)>();
+
+var turn = 1;
+var currentPlayerIndex = 0;
+
+var players = new Player[3]
+{
+    new Player("DonaldTrump"),
+    new Player("Elhvind"),
+    new Player("Mivsen"),
+};
+
+while (players.Count(p => p.Alive) > 1)
+{
+    PrintMenu(true);
+
+    Console.Write("Select Action: ");
+
+    var currentPlayer = players[currentPlayerIndex];
+    var alivePlayers = players.Where(x => x.Alive && x.Name != currentPlayer.Name);
+
+    string action;
+
+    switch (Console.ReadKey())
+    {
+        case { Key: ConsoleKey.A }:
+            Console.WriteLine();
+            for (int i = 0; i < players.Length; i++)
+            {
+                var player = players[i];
+                if (player.Alive && currentPlayerIndex != i)
+                {
+                    Console.WriteLine($"{i + 1}: {player.Name}");
+                }
+            }
+
+            Console.Write("Select player to attack: ");
+            int.TryParse(Console.ReadLine(), out var selectedPlayerIndex);
+
+            var selectedPlayer = players[selectedPlayerIndex - 1];
+
+            action = currentPlayer.Attack(selectedPlayer);
+            break;
+
+        case { Key: ConsoleKey.H }:
+            Console.WriteLine();
+            action = currentPlayer.Heal();
+            break;
+
+        default:
+            continue;
+    }
+
+    consoleLog.Add((turn, action));
+
+    Console.WriteLine();
+    Console.WriteLine(action);
+    Console.WriteLine();
+    Console.WriteLine("Press <Enter> to end turn!");
+    Console.ReadLine();
+
+    AdvancePlayerTurn();
+}
+
+PrintMenu(false);
+
+void AdvancePlayerTurn()
+{
+    var playerCount = players!.Length;
+    if (playerCount > 1)
+        currentPlayerIndex += 1;
+
+    if (currentPlayerIndex == playerCount)
+        currentPlayerIndex = 0;
+
+    turn += 1;
+}
+
+void PrintMenu(bool showActions)
+{
+    const string divider = """---------------------------""";
+
+    Console.Clear();
+    Console.WriteLine(divider);
+
+    for (int i = 0; i < players.Length; i++)
+    {
+        Console.WriteLine($"""
+            Player {i + 1} {(currentPlayerIndex == i ? "(Active)" : "")}
+            Name: {players[i].Name}
+            HP:   {players[i].Health:00}
+            {divider}
+            """);
+    }
+
+    if (showActions)
+    {
+        Console.WriteLine($"""
+            Actions:
+            A - Attack
+            H - Heal
+            {divider}
+            """);
+    }
+    else
+    {
+        foreach (var consoleLogItem in consoleLog)
+        {
+            Console.WriteLine($"{consoleLogItem.turn}: {consoleLogItem.action}");
+        }
+    }
+}
